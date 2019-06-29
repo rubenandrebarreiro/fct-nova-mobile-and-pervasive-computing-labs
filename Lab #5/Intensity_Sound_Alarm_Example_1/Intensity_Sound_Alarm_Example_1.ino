86b685c2c827ac7c9d7ed2e67287919f65434e0f
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-06-29 17:42:58 +0100

    Some Labs added and modified
    Some Labs added and modified

--- Lab #5/Intensity_Sound_Alarm_Example_1/Intensity_Sound_Alarm_Example_1.ino
+++ Lab #5/Intensity_Sound_Alarm_Example_1/Intensity_Sound_Alarm_Example_1.ino
@@ -1,5 +1,17 @@
-// A sketch to demonstrate the tone() function
+/*
+ * Simple Sketch to test some PIEZO sound alarms,
+ * with several tones' delays and intensities.
+ * 
+ * Adapted for the Pratical Labs (Lab #5 - April 11, 2019) of:
+ * - Mobile and Pervasive Computing
+ *   @ FCT NOVA (Faculty of Sciences and Technology of New University of Lisbon)
 
+ * Adapted by:
+ * - Carmen Pires Morgado - cpm@fct.unl.pt
+ * - Ruben Andre Barreiro - r.barreiro@campus.fct.unl.pt
+ */
+ 
+// A sketch to demonstrate the tone() function
 // Specify digital pin on the Arduino that the positive lead of piezo buzzer is attached.
 int piezoPin = 6;
 
--- Lab #5/RGB_Color_Spectrum_Example_1/RGB_Color_Spectrum_Example_1.ino
+++ Lab #5/RGB_Color_Spectrum_Example_1/RGB_Color_Spectrum_Example_1.ino
@@ -32,6 +32,14 @@
   Visit http://www.arduino.cc to learn about the Arduino.
   
   Version 2.0 6/2012 MDG
+
+  Adapted for the Pratical Labs (Lab #5 - April 11, 2019) of:
+  - Mobile and Pervasive Computing
+    @ FCT NOVA (Faculty of Sciences and Technology of New University of Lisbon)
+
+  Adapted by:
+  - Carmen Pires Morgado - cpm@fct.unl.pt
+  - Ruben Andre Barreiro - r.barreiro@campus.fct.unl.pt
 */
 
 
